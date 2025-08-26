dde4869c736540628f6820eb5ba75328a442b59a
BEN <freefire1379045919@gmail.com>
2025-08-26 17:35:25 +0530

    Create __init__.py

---
+++ alex/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "1.0.0"
+__author__ = "Alex"
+__description__ = "A romantic Telegram bot designed for couples with AI chatbot features"
+
+from . import config
+from . import modules
+from . import plugins
